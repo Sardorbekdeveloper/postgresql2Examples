1879971a1e01f578debe8ad45de796e86c1a03d7
Sardorbekdeveloper <davletboyevsardorbek08@gmail.com>
2026-06-22 22:42:28 +0500

    change

--- 2-task.sql
+++ 2-task.sql
@@ -1,20 +1,38 @@
 /* INDEX
 
 Topshiriq:
-
 Talabalar jadvalida student_id ustunini,
-O‘qituvchilar jadvalida teacher_id ustunini indekslang.
+O‘qituvchilar jadvalida teacher_id ustunini indekslang.*/
+
+CREATE INDEX idx_student_id
+ON student(student_id);
+
+CREATE INDEX idx_teacher_id
+ON teacher(teacher_id);
+
+/*
 ORDER BY va LIMIT
-
 Topshiriq:
-
 Har bir kurs bo‘yicha 10 ta eng yaxshi talabaning ismini va baholarini tartibda qaytaring.
+*/
+SELECT firstname, grade
+FROM student
+WHERE course_id = 1
+ORDER BY grade DESC
+LIMIT 10;
 
+/*
 GROUP BY va HAVING
-
 Topshiriq:
-
 Har bir o‘quvchi bo‘yicha o‘rtacha bahosi 85 dan yuqori bo‘lgan kurslarni toping va natijalarni guruhlang.
+*/
+SELECT course_id,
+       AVG(grade) AS average_grade
+FROM student
+GROUP BY coursename
+HAVING AVG(grade) > 85;
+
+
 
 UNION va AS
 
